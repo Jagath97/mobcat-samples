00c9f2637e97903630a6257b875a8f96adf830bb
Jagath Kumar Vijayakumar <JVijayakumar@slb.com>
2022-09-01 14:56:46 +0530

    sss

--- notification_hub_backend_service/src/azure/PushDemoApi/PushDemoApi/Authentication/ApiKeyAuthOptions.cs
+++ notification_hub_backend_service/src/azure/PushDemoApi/PushDemoApi/Authentication/ApiKeyAuthOptions.cs
@@ -4,7 +4,7 @@ namespace PushDemoApi.Authentication
 {
     public class ApiKeyAuthOptions : AuthenticationSchemeOptions
     {
-        public const string DefaultScheme = "ApiKey";
+        public const string DefaultScheme = "3a22964e-f531-4c8c-a705-f7a4317ef1ac";
         public string Scheme => DefaultScheme;
         public string ApiKey { get; set; }
     }
